7e2950072b0c8616252d44376944252751338291
임동혁 <idh1007@naver.com>
2022-09-30 19:25:09 +0900

    .

--- hw2/main.py
+++ hw2/main.py
@@ -1,43 +1,61 @@
-import cv2
 import numpy as np
+import cv2 as cv
 
-ori_img1 = cv2.imread('set1/06.png')
-ori_img2 = cv2.imread('set1/07.png')
-ori_img3 = cv2.imread('set1/08.png')
-
-img1 = cv2.cvtColor(ori_img1, cv2.COLOR_BGR2GRAY)
-img2 = cv2.cvtColor(ori_img3, cv2.COLOR_BGR2GRAY)
-img3 = cv2.cvtColor(ori_img3, cv2.COLOR_BGR2GRAY)
-
-detector = sift = cv2.xfeatures2d.SIFT_create(sigma=1.0)
-
-keypoints1, descriptor1 = detector.detectAndCompute(img1, None)
-keypoints2, descriptor2 = detector.detectAndCompute(img2, None)
-matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_FLANNBASED)
-knn_matches = matcher.knnMatch(descriptor1, descriptor2, 2)
-
-ratio_thresh = 0.7
-good_matches=[]
-for m, n in knn_matches:
-    if m.distance < ratio_thresh * n.distance:
-        good_matches.append(m)
-
-obj = np.empty((len(good_matches),2), dtype=np.float32)
-scene = np.empty((len(good_matches),2), dtype=np.float32)
-for i in range(len(good_matches)):
-    obj[i,0] = keypoints1[good_matches[i].queryIdx].pt[0]
-    obj[i,1] = keypoints1[good_matches[i].queryIdx].pt[1]
-    scene[i,0] = keypoints2[good_matches[i].trainIdx].pt[0]
-    scene[i,1] = keypoints2[good_matches[i].trainIdx].pt[1]
+FLANN_INDEX_LSH = 6
 
 
-H, _ = cv2.findHomography(scene, obj, cv2.RANSAC)
+def main(img1, img2):
+    gray1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+    gray2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
 
-output_img = cv2.warpPerspective(ori_img1,H,
-                                 (ori_img1.shape[1] + ori_img2.shape[1],
-                                 max(ori_img1.shape[0], ori_img2.shape[0])))
+    detector = cv.BRISK_create()
+    keyPoints1, descriptors1 = detector.detectAndCompute(gray1, None)
+    keyPoints2, descriptors2 = detector.detectAndCompute(gray2, None)
+    print('img1 - %d features, img2 - %d features' % (len(keyPoints1), len(keyPoints2)))
 
-output_img[0:ori_img1.shape[0], 0:ori_img1.shape[1]] = ori_img1
-cv2.imshow("result", output_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    keyPoints1 = np.float32([keypoint.pt for keypoint in keyPoints1])
+    keyPoints2 = np.float32([keypoint.pt for keypoint in keyPoints2])
+
+    flann_params = dict(algorithm=FLANN_INDEX_LSH,
+                        table_number=6,  # 12
+                        key_size=12,  # 20
+                        multi_probe_level=1)  # 2
+
+    matcher = cv.FlannBasedMatcher(flann_params, {})  # bug : need to pass empty dict (#1329)
+    raw_matches = matcher.knnMatch(descriptors1, descriptors2, k=2)  # 2
+
+    matches = []
+    for m in raw_matches:
+        if len(m) == 2 and m[0].distance < m[1].distance * 0.79:
+            matches.append((m[0].trainIdx, m[0].queryIdx))
+
+
+    keyPoints1 = np.float32([keyPoints1[i] for (_, i) in matches])
+    keyPoints2 = np.float32([keyPoints2[i] for (i, _) in matches])
+
+    H, status = cv.findHomography(keyPoints1, keyPoints2, cv.RANSAC, 4.0)
+
+
+
+    result = cv.warpPerspective(img1, H,
+                                (img1.shape[1] + img2.shape[1], img1.shape[0]))
+    result[0:img2.shape[0], 0:img2.shape[1]] = img2
+    return result
+
+
+if __name__ == '__main__':
+    # img1 = cv.imread('set1/08.png')
+    # img2 = cv.imread('set1/07.png')
+    # img3 = cv.imread('set1/06.png')
+
+    img1 = cv.imread('set2/004.png')
+    img2 = cv.imread('set2/003.png')
+    img3 = cv.imread('set2/002.png')
+    img4 = cv.imread('set2/001.png')
+
+    result = main(img1,img2)
+    result = main(result, img3)
+    result = main(result, img4)
+    cv.imshow('result', result)
+    cv.waitKey()
+    cv.destroyAllWindows()
